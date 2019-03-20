962d8aee90d2e29874250eb8ce7b7761d038d47e
Ezekiel De Leon <ezekieldeleon@ezekiels-mbp-2.attlocal.net>
2019-03-20 16:55:20 -0700

    Finished some algo.

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,47 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// Time complexity is O(n*log(n)) since we are using a sort.
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB);
+}
 
+function cleanString(str) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+
+
+/*
+//Solution 1: The time complexity here is O(n) since we are only looping once on each word.
+function anagrams(stringA, stringB) {
+    let word1 = stringA.replace(/[^\w]/g, '').toLowerCase().split('');
+    let word2 = stringB.replace(/[^\w]/g, '').toLowerCase().split('');
+    const mapA = new Map();
+    const mapB = new Map();
+    var flag = true;
+
+    if (word1.length !== word2.length) {
+        return false
+    } else {
+        for (var char of word1) {
+            if (mapA.has(char)) {
+                mapA.set(char, mapA.get(char) + 1);
+            } else {
+                mapA.set(char, 1);
+            }
+        }
+        for (var char of word2) {
+            if (mapB.has(char)) {
+                mapB.set(char, mapB.get(char) + 1);
+            } else {
+                mapB.set(char, 1);
+            }
+        }
+        for (var [key, value] of mapA.entries()) {
+            if (!mapB.has(key) && mapB.get(key) !== value) return false;
+        }
+        return true;
+    }
+}
+*/
 module.exports = anagrams;
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,10 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    var sentence = str.split(' ');
+    sentence = sentence.map(word => word[0].toUpperCase() + word.slice(1));
+    return sentence.join(' ');
+}
 
 module.exports = capitalize;
